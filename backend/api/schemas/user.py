0e6cad154020d368e070f9f134bc980d15c95288
jaydenrrr22 <sryu@charlotte.edu>
2026-04-02 18:38:35 -0400

    Add select option for a dataset during account creation

--- backend/api/schemas/user.py
+++ backend/api/schemas/user.py
@@ -1,9 +1,12 @@
+from typing import Optional
+
 from pydantic import BaseModel, EmailStr
 
 class UserCreate(BaseModel):
     name: str
     email: EmailStr
     password: str
+    dataset_id: Optional[int] = None
 
 class UserLogin(BaseModel):
     email: EmailStr
