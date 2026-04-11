035cc045c3930d4364faf6516b76aeafe095f0fd
Alana <rgreen.alana@gmail.com>
2026-04-11 10:33:06 -0400

    Converted csv file into sql

---
+++ data_analysis/churn_data_top20.sql
@@ -0,0 +1,28 @@
+CREATE TABLE churn_data (
+  `id` FLOAT,
+  `cost` FLOAT,
+  `category` VARCHAR(255),
+  `store_name` VARCHAR(255),
+  `date` VARCHAR(255)
+);
+
+INSERT INTO churn_data VALUES (2.0, 932.0, 'Video Streaming', 'Max', '2026-03-27');
+INSERT INTO churn_data VALUES (3.0, 557.0, 'Music Streaming', 'Apple Music', '2026-04-01');
+INSERT INTO churn_data VALUES (4.0, 185.0, 'Fitness', 'Strava Premium', '2026-04-05');
+INSERT INTO churn_data VALUES (5.0, 396.0, 'Shopping Membership', 'Walmart+', '2026-03-23');
+INSERT INTO churn_data VALUES (6.0, 617.0, 'Music Streaming', 'Spotify', '2026-03-18');
+INSERT INTO churn_data VALUES (8.0, 129.0, 'Video Streaming', 'Disney+', '2026-04-05');
+INSERT INTO churn_data VALUES (9.0, 821.0, 'Video Streaming', 'Hulu', '2026-03-20');
+INSERT INTO churn_data VALUES (10.0, 445.0, 'Productivity', 'Microsoft 365', '2026-03-18');
+INSERT INTO churn_data VALUES (11.0, 969.0, 'Music Streaming', 'Apple Music', '2026-03-25');
+INSERT INTO churn_data VALUES (12.0, 415.0, 'Video Streaming', 'Disney+', '2026-03-15');
+INSERT INTO churn_data VALUES (13.0, 930.0, 'Fitness', 'Peloton', '2026-03-21');
+INSERT INTO churn_data VALUES (14.0, 830.0, 'Video Streaming', 'Disney+', '2026-03-25');
+INSERT INTO churn_data VALUES (15.0, 265.0, 'Shopping Membership', 'Amazon Prime', '2026-03-29');
+INSERT INTO churn_data VALUES (16.0, 518.0, 'Video Streaming', 'Max', '2026-03-27');
+INSERT INTO churn_data VALUES (17.0, 204.0, 'Shopping Membership', 'Amazon Prime', '2026-04-11');
+INSERT INTO churn_data VALUES (18.0, 975.0, 'Shopping Membership', 'Walmart+', '2026-04-04');
+INSERT INTO churn_data VALUES (19.0, 978.0, 'Video Streaming', 'Disney+', '2026-04-10');
+INSERT INTO churn_data VALUES (20.0, 151.0, 'Shopping Membership', 'Walmart+', '2026-04-02');
+INSERT INTO churn_data VALUES (21.0, 669.0, 'Video Streaming', 'Hulu', '2026-03-31');
+INSERT INTO churn_data VALUES (22.0, 262.0, 'Shopping Membership', 'Amazon Prime', '2026-04-05');
